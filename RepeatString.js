eb2ae463df5e96ec01ac22dc285fec1ea581f6ad
StanislavHrusanov <102349333+StanislavHrusanov@users.noreply.github.com>
2022-05-09 07:44:02 +0300

    Add problem solution

---
+++ RepeatString.js
@@ -0,0 +1,8 @@
+function repeatString(string, n) {
+    let newString = '';
+    for (let i = 0; i < n; i++) {
+        newString += string;
+    }
+    return (newString);
+}
+console.log(repeatString("abc", 3));
